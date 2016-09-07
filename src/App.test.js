7262e2f74fa8e7cf5b78a13cc5d1cc87ee0c03a4
Nate Norberg <natenorberg@gmail.com>
2016-09-07 12:29:25 -0600

    inital commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as Constants from 'constants';
+import * as OtherModule from 'otherModule';
 import * as RelativeConstants from './constants';
 
 it('renders without crashing', () => {
@@ -17,4 +18,9 @@ it('loads constants', () => {
 it('loads constants from relative path', () => {
   //console.log(RelativeConstants);
   expect(RelativeConstants.TEST_CONSTANT).toBe('HELLO_WORLD');
+});
+
+it('loads other modules', () => {
+  //console.log(OtherModule);
+  expect(OtherModule.SOME_VALUE).toBe('asdf');
 });
